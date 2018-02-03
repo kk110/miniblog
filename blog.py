6ac69d13feacd654b9e8b33e4811b322da10c7e9
xwp <xwp_fullstack@163.com>
2018-02-03 11:08:04 +0800

    初定模型类，解决不能运行shell

--- blog.py
+++ blog.py
@@ -3,8 +3,9 @@ from flask import Flask,session
 from flask_script import Manager
 from flask_migrate import MigrateCommand,Migrate
 from myblog import create_app,db
-from flask import render_template
-from datetime import datetime
+# from flask import render_template
+# from datetime import datetime
+# from flask.ext.script import Shell  #集成shell
 
 
 app = create_app('developement')
@@ -16,16 +17,21 @@ app = create_app('developement')
 # def page_not(e):
 #     return render_template('404.html'),404
 
-@app.route('/')
-def index():
-    return render_template('index.html',current_time=datetime.utcnow())
+# @app.route('/')
+# def index():
+#     return render_template('index.html',current_time=datetime.utcnow())
+
 
 # 数据库迁移和扩展
 manager = Manager(app)
 Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 
+@manager.shell
+def make_shell_context():
+    return dict(app=app)
+
 
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
--- myblog/models.py
+++ myblog/models.py
@@ -2,15 +2,30 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 
+
+class Role(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(64),unique=True)
+    is_admin = db.Column(db.Boolean, default=False, nullable=False)
+    permissions = db.Column(db.Integer)
+    user = db.relationship('User',backref='role',lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>'%self.name
+
 class User(db.Model):
     '''用户'''
 
     __tablename__ = 'blog_user'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(32), unique=True, nullable=False)
+    username = db.Column(db.String(32), unique=True, nullable=False,index=True)
+    email = db.Column(db.String(64),unique=True,nullable=False,index=True)
     password_hash = db.Column(db.String(128),nullable=False)
-    is_admin = db.Column(db.Boolean(),nullable=False)
+    confirmed = db.Column(db.Boolean,default=False)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+
+
 
     @property
     def password(self):
